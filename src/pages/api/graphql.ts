9f6903d6a343215937f061b6548f3c1ce2ca8951
break7533 <ll61295@gmail.com>
2019-11-09 14:59:50 +0000

    Apollo server  micro

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,11 +1,37 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { ApolloServer, gql } from "apollo-server-micro";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  req.headers;
-  res.status(200).json({
-    test: "Hello level up"
-  });
+const typeDefs = gql`
+  type Query {
+    sayHello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    sayHello: () => {
+      return "Hello World!";
+    }
+  }
 };
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
+
+export default apolloServer.createHandler({ path: "/api/graphql" });
+
+// import { NextApiRequest, NextApiResponse } from "next";
+
+// export default (req: NextApiRequest, res: NextApiResponse) => {
+//   req.headers;
+//   res.status(200).json({
+//     test: "Hello level up"
+//   });
+// };
 
 // Long way
 // export default (req, res) => {
