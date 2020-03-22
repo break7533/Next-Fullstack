fb669c645e76f3065942de85c2caff7613f73213
break7533 <ll61295@gmail.com>
2020-03-22 20:19:46 +0000

    Set up for Apollo Client

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,10 +1,36 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { ApolloServer, gql } from 'apollo-server-micro';
 
-export default (req: NextApiRequest, res: NextApiResponse): void => {
-    res.status(200).json({
-        test: 'Hello Leonor',
-    });
+const typeDefs = gql`
+  type Query {
+    sayHello: String
+  }
+`;
+
+const resolvers = {
+    Query: {
+        sayHello: (): string => {
+            return 'Hello World!';
+        }
+    }
 };
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+export const config = {
+    api: {
+        bodyParser: false
+    }
+};
+
+export default apolloServer.createHandler({ path: '/api/graphql' });
+
+// import { NextApiRequest, NextApiResponse } from 'next';
+
+// export default (req: NextApiRequest, res: NextApiResponse): void => {
+//     res.status(200).json({
+//         test: 'Hello Leonor',
+//     });
+// };
 
 // Long way
 // export default (req, res) => {
